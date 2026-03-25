a7b410ad169eda3513c3e6d5a8e5e3b342813c8f
TuNombreDeUsuario <tu-email@ejemplo.com>
2026-03-25 12:43:52 -0300

    Agregado script de construcción para Render

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+# Salir si hay un error
 set -o errexit
+
+# Instalar dependencias
 pip install -r requirements.txt
+
+# Recolectar archivos estáticos (CSS, Imágenes)
 python manage.py collectstatic --no-input
+
+# Aplicar migraciones de la base de datos
 python manage.py migrate
