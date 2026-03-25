dbd63721471589451eaf17122c1dd55e9c9d419d
TuNombreDeUsuario <tu-email@ejemplo.com>
2026-03-25 16:25:46 -0300

    Fix final: Corregida ruta de admin y media para Render

--- config/urls.py
+++ config/urls.py
@@ -1,16 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from catalogo.views import home, agregar_al_carrito, ver_carrito, limpiar_carrito
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('agregar/<int:producto_id>/', agregar_al_carrito, name='agregar'),
-    path('carrito/', ver_carrito, name='ver_carrito'),
-    path('limpiar/', limpiar_carrito, name='limpiar_carrito'),
+    path('admin/', admin.site.urls), # Corregido: .urls en lugar de .path
+    path('', include('catalogo.urls')),
 ]
 
-if settings.DEBUG:
+# Esto sirve las imágenes en Render (cuando DEBUG es False)
+if not settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
