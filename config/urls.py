5a5aed15b476bb1217dd5dc8d4150ae30fbc25c3
TuNombreDeUsuario <tu-email@ejemplo.com>
2026-03-25 18:53:35 -0300

    Fix: Forzando servicio de imagenes media en produccion

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalogo.urls')),
+    
+    # Esta es la linea mágica para que las fotos aparezcan en Render
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+    path('static/<path:path>', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
-if not settings.DEBUG:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
